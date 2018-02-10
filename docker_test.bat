3844247f5befcced1cad53bb0e3976c520a22902
Mats Skoglund <mats.skoglund@scania.com>
2018-02-10 18:13:58 +0100

    Trying to change the service

---
+++ docker_test.bat
@@ -0,0 +1,8 @@
+docker network create devkitapinet
+docker network ls
+docker run --net=devkitapinet --name devkitapidb -e MYSQL_ROOT_PASSWORD=mypass -d mariadb
+docker build -t matsskoglund/devkitapi:latest .
+docker run -e ASPNETCORE_ENVIRONMENT=Staging --net=devkitapinet --name devkitapi -d -p 5000:5000 matsskoglund/devkitapi:latest
+newman run https://raw.githubusercontent.com/matsskoglund/DevkitApi/master/test/DevkitApi.Test.postman_collection.json
+docker stop devkitapidb
+docker network rm devkitapinet
